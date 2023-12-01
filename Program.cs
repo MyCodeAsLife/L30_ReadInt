5fe828b9e2eae80664a1e42d23685376b0c3057d
Vladimir <w112vovan@mail.ru>
2023-12-01 13:49:20 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L30_ReadInt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int userNumber = NumberRequest();
+
+            Console.WriteLine($"\nПользователь ввел число: {userNumber}");
+        }
+
+        static int NumberRequest()
+        {
+            int number = 0;
+            bool correctAction = false;
+
+            while (correctAction == false)
+            {
+                Console.Clear();
+                Console.Write("Введите число: ");
+                correctAction = int.TryParse(Console.ReadLine(), out number);
+            }
+
+            return number;
+        }
+    }
+}
